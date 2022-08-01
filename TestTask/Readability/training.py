6322bd14b99305211a774941acb10acd787234f8
lyabomyr <68542310+lyabomyr@users.noreply.github.com>
2022-08-01 13:02:51 +0300

    Update training.py

--- TestTask/Readability/training.py
+++ TestTask/Readability/training.py
@@ -24,12 +24,10 @@ class TrainML():
             model.fit(train_X, train_y)
             train_preds = model.predict(train_X)
             train_e = np.sqrt(mean_absolute_error(train_y, train_preds))
-            # print(f'result for mean_absolute_error for train error MSE = {train_e}')
             val_preds = model.predict(val_X)
             val_e = mean_absolute_error(val_y, val_preds)
             confusion_matrix.loc[max_leaf_nodes - 2] = [train_e, val_e,
                                                         model.get_n_leaves()]  # train_e = MSE for train_error,\
-            # val_e = MSE for val_error #get_n_leaves() = number of leaves of the decision tree
 
 
 
@@ -40,15 +38,7 @@ class TrainML():
 
         model = DecisionTreeRegressor(random_state=0, max_leaf_nodes=24)
         model.fit(train_X, train_y)
-        #y_pred = model.predict(X)
-
-        #print(f'first 5 confusion matrix param for example{confusion_matrix.head()}')
-        #print(f'MSE for train error {confusion_matrix.train_error.mean()}')
-        #print(f'MSE for val error {confusion_matrix.val_error.mean()}')
-        #print(f' for simple train r2 score {r2_score(y, y_pred)}')
-
-        model = DecisionTreeRegressor(random_state=0, max_leaf_nodes=24)
-        model.fit(train_X, train_y)
+        
 
 
         print("r2_score = %s" % r2_score(train_y, train_preds))
